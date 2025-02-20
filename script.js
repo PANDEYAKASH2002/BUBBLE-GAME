76a68b73559574706df23fce9fca53e542dc346a
AKASH PANDEY <157959012+PANDEYAKASH2002@users.noreply.github.com>
2025-02-20 13:34:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+var hitrn=0;
+function makeBubble(){
+    var clutter= "";
+
+for (var i=1; i<=112;i++){
+    var rn = Math.floor(Math.random()*10)
+    clutter += ` <div class="bubble">${rn}</div>`
+}
+document.querySelector("#pbtm").innerHTML=clutter;
+}
+var timer =6;
+function runTimer(){
+      var cleartimer=  setInterval(function(){
+            if (timer>0){
+            timer--;
+            document.querySelector("#timervalue").innerHTML = timer;
+        }
+        else{
+            clearInterval(cleartimer);
+            document.querySelector("#pbtm").innerHTML=`<h1>GAME OVER
+            <span><h6 >YOUR SCORE IS ${score}</h6></span></h1>`;
+        }
+        },1000);
+
+}
+function getNewHit(){
+    hitrn= Math.floor(Math.random()*10)
+    document.querySelector("#hitval").textContent = hitrn;
+}
+var score=0;
+function increaseScore(){
+    score += 10;
+    document.querySelector("#scoreval").textContent = score;
+}
+document.querySelector("#pbtm").addEventListener("click",function(details){
+   var clickednum = (Number(details.target.textContent));
+   if(clickednum === hitrn){
+    increaseScore();
+    makeBubble();
+    getNewHit();
+   }
+})
+getNewHit();
+runTimer();
+makeBubble();
